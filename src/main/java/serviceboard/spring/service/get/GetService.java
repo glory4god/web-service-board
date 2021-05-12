b6bd2ea379a5df5c0519bc9a3c3298bb1b53431c
Hayoung <gkduddy0814@gmail.com>
2021-05-12 15:05:03 +0900

    feat : add get service complete in postman

--- src/main/java/serviceboard/spring/service/get/GetService.java
+++ src/main/java/serviceboard/spring/service/get/GetService.java
@@ -3,8 +3,10 @@ package serviceboard.spring.service.get;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import serviceboard.spring.domain.post.Post;
 import serviceboard.spring.domain.post.PostRepository;
 import serviceboard.spring.web.dto.PostListResponseDto;
+import serviceboard.spring.web.dto.PostResponseDto;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -12,13 +14,19 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 @Service
 public class GetService {
-    private PostRepository postRepository;
+    private final PostRepository postRepository;
 
     @Transactional(readOnly = true)
     public List<PostListResponseDto> findAll() {
         return postRepository.findAll().stream()
                 .map(PostListResponseDto::new)
                 .collect(Collectors.toList());
+    }
+
+    public PostResponseDto findById(Long id) {
+        Post entity = postRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시물이 없습니다. id=" + id));
+        return new PostResponseDto(entity);
     }
 
 }
--- src/main/java/serviceboard/spring/web/GetApiController.java
+++ src/main/java/serviceboard/spring/web/GetApiController.java
@@ -2,9 +2,11 @@ package serviceboard.spring.web;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import serviceboard.spring.service.get.GetService;
 import serviceboard.spring.web.dto.PostListResponseDto;
+import serviceboard.spring.web.dto.PostResponseDto;
 
 import java.util.List;
 
@@ -12,10 +14,16 @@ import java.util.List;
 @RestController
 public class GetApiController {
 
-    private GetService getService;
+    private final GetService getService;
+
+    @GetMapping("/api/board/{id}")
+    public PostResponseDto findById(@PathVariable Long id) {
+        return getService.findById(id);
+    }
 
     @GetMapping("/api/board")
     public List<PostListResponseDto> findAll() {
+
         return getService.findAll();
     }
 
